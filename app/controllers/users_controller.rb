65016fcbdfbe288bdb961d500c1ee9f9d0ad2e28
Erika Hughes <ehughes@knights.ucf.edu>
2017-05-04 18:41:45 +0000

    set up users and games database, index and signup views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect '/'
+    end
+    erb :'users/new_user'
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+    end
+    @user = User.create(:username => params[:username], :password => params[:password])
+    session[:id] = @user.id
+    redirect '/'
+  end
 end
